e0c684a4922211c54bc5c94e148e5a411e19ade9
Yovindu <yovindu.20200870@iit.ac.lk>
2022-03-11 13:18:45 +0530

    added data prediction model for the learning styles

--- ds-component/main.py
+++ ds-component/main.py
@@ -1,5 +1,9 @@
 from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
 from keras.utils.np_utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Dense
+from matplotlib import pyplot
 import csv
 import numpy as np
 
@@ -20,6 +24,9 @@ def prepare_data(model_type):
     le = LabelEncoder()
     encoded_Y = le.fit_transform(y)
     y = to_categorical(encoded_Y)
+
+    list_x = np.array(x)
+    x = list_x.astype(np.int)
 
     n_train = round(len(y)*0.67)
     trainy = []
@@ -43,12 +50,28 @@ def prepare_data(model_type):
     testX = np.array(testX)
     trainy = np.array(trainy)
     testy = np.array(testy)
-    print(trainX)
     print("Finished data preparation...........")
     return trainX, trainy, testX, testy, le
 
+def learning_model(trainX, trainy, testX, testy):
+    model = Sequential()
+    model.add(Dense(12, input_dim=10, activation='relu'))
+    model.add(Dense(8, activation='relu'))
+    model.add(Dense(4, activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    print(model.summary())
+    history = model.fit(trainX, trainy, validation_data=(testX, testy),batch_size=1, epochs=200, verbose=1)
+
+    pyplot.plot(history.history['accuracy'], label='train')
+    pyplot.plot(history.history['val_accuracy'], label='test')
+    scores = model.evaluate(trainX, trainy, verbose=1)
+    print("%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
+    model.save("model.h5")
+    print("Model saved to disk")
+
 if __name__ == "__main__":
     trainX, trainy, testX, testy, le = prepare_data("Learning-Style")
-    #model train
     #trainX, trainy, testX, testy, le = prepare_data("Personality-Trait")
+    learning_model(trainX, trainy, testX, testy)
+
 
