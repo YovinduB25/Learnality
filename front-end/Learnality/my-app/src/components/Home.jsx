b1c6a85443a8651911cfc4a344d58083eb285d93
Mathushaa Vigneswararajah <98661558+Mathushaan26@users.noreply.github.com>
2022-04-29 00:10:44 +0530

    Front-end updated with chat-bot

--- front-end/Learnality/my-app/src/components/Home.jsx
+++ front-end/Learnality/my-app/src/components/Home.jsx
@@ -6,6 +6,20 @@ import LearningStylePic from "../images/HomePage/LearningStylePic.png";
 import { SideBar } from "../components/SideBar";
 
 export class Home extends React.Component{
+
+    componentDidMount() {
+        
+        (function(d, m){
+            var kommunicateSettings = 
+                {"appId":"1903feb6bdd7ce29e3a1447976193ff7","popupWidget":true,"automaticChatOpenOnNavigation":true};
+            var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+            s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+            var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+            window.kommunicate = m; m._globals = kommunicateSettings;
+        })(document, window.kommunicate || {});
+    /* NOTE : Use web server to view HTML files as real-time update will not work if you directly open the HTML file in the browser. */
+          
+}
     render(){
 
         // https://stackoverflow.com/questions/67030873/refresh-the-page-only-once-in-react-class-component
