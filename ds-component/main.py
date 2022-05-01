283b724a093594815362f8bda810c75eb2a6552c
Yovindu <yovindu.20200870@iit.ac.lk>
2022-05-01 16:16:19 +0530

    Comments added

--- ds-component/main.py
+++ ds-component/main.py
@@ -10,28 +10,31 @@ from flask import Flask, request
 import json
 import ast
 
-def prepare_data(model_type):
-    print("Started data preparation...........")
-    csvFile = open('responses.csv', 'r')
-    features = csv.reader(csvFile, delimiter=',')
-    y = []
-    x = []
-    for line in features:
-        if(model_type == "Learning-Style" ):
-            x.append(line[4:14])
-            y.append(line[-2])
-        else:
-            x.append(line[14:24])
-            y.append(line[-1])
 
-    le = LabelEncoder()
-    encoded_Y = le.fit_transform(y)
+def prepare_data(model_type):  # pre-processing step for data preparation
+    # https://www.tutorialspoint.com/machine_learning_with_python/machine_learning_with_python_preparing_data.htm#:~:text=Another%20useful%20data%20preprocessing%20technique,of%20scikit%2Dlearn%20Python%20library.
+    print("Started data preparation...........")
+    csvFile = open('responses.csv', 'r')  # opening csv file
+    features = csv.reader(csvFile, delimiter=',')
+    y = []  # initializing y list for prediction data
+    x = []  # initializing x list for data
+    for line in features:
+        if (model_type == "Learning-Style"):  # adding values to learning model
+            x.append(line[4:14])  # 4th column to 14th will add to learning model
+            y.append(line[-2])  # 25th column will add as user's opinion
+        else:
+            x.append(line[14:24])  # 14th column to 24th will add to personal model
+            y.append(line[-1])  # 26th column will add as user's opinion
+
+    le = LabelEncoder()  # https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.LabelEncoder.html
+    encoded_Y = le.fit_transform(y)  # fit label encoder to y list
     y = to_categorical(encoded_Y)
 
     list_x = np.array(x)
-    x = list_x.astype(np.int)
+    x = list_x.astype(np.int)  # converting x values in to integers https://stackoverflow.com/questions/3877209/how-to-convert-an-array-of-strings-to-an-array-of-floats-in-numpy
 
-    n_train = round(len(y)*0.67)
+    n_train = round(len(y) * 0.67)  # 2/3 of data using to training model
+    # initialising tarinX, testX, testY, testY
     trainy = []
     testy = []
     trainX = []
@@ -39,69 +42,75 @@ def prepare_data(model_type):
 
     for line in y:
         if len(trainy) < n_train:
-            trainy.append(line)
+            trainy.append(line)  # appending values into tarinY
         else:
-            testy.append(line)
+            testy.append(line)  # appending values into testY
 
     for line in x:
         if len(trainX) < n_train:
-            trainX.append(line)
+            trainX.append(line)  # appending values into tarinX
         else:
-            testX.append(line)
+            testX.append(line)  # appending values into testX
 
-    trainX = np.array(trainX)
+    trainX = np.array(trainX)   # https://numpy.org/doc/stable/reference/generated/numpy.array.html
     testX = np.array(testX)
     trainy = np.array(trainy)
     testy = np.array(testy)
     print("Finished data preparation...........")
     return trainX, trainy, testX, testy, le
 
+# https://keras.io/guides/sequential_model/
 def learning_model(trainX, trainy, testX, testy):
-    model = Sequential()
+    model = Sequential()    # https://www.tensorflow.org/api_docs/python/tf/keras/Sequential
     model.add(Dense(100, input_dim=10, activation='relu'))
-    model.add(Dense(24, activation='relu'))
+    model.add(Dense(24, activation='relu'))  # https://machinelearningmastery.com/rectified-linear-activation-function-for-deep-learning-neural-networks/
     model.add(Dense(4, activation='softmax'))
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])  # https://www.tutorialspoint.com/keras/keras_model_compilation.htm
     print(model.summary())
-    history = model.fit(trainX, trainy, validation_data=(testX, testy),batch_size=1, epochs=100, verbose=1)
+    history = model.fit(trainX, trainy, validation_data=(testX, testy), batch_size=1, epochs=100, verbose=1)
 
-    pyplot.plot(history.history['accuracy'], label='train')
-    pyplot.plot(history.history['val_accuracy'], label='test')
+    pyplot.plot(history.history['accuracy'], label='train')  # https://matplotlib.org/3.1.1/api/_as_gen/matplotlib.pyplot.html#:~:text=pyplot%20is%20a%20state%2Dbased,%2C%200.1)%20y%20%3D%20np.
+    pyplot.plot(history.history['val_accuracy'], label='test')  # https://www.geeksforgeeks.org/pyplot-in-matplotlib/
     pyplot.show()
     scores = model.evaluate(trainX, trainy, verbose=1)
     print("%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
     model.save("learning_model.h5")
     print("Model saved to disk")
 
+#https://keras.io/guides/sequential_model/
 def personality_model(trainX, trainy, testX, testy):
-    model = Sequential()
+    model = Sequential()    # https://www.tensorflow.org/api_docs/python/tf/keras/Sequential
     model.add(Dense(100, input_dim=10, activation='relu'))
-    model.add(Dense(50, activation='relu'))
+    model.add(Dense(50, activation='relu'))  # https://machinelearningmastery.com/rectified-linear-activation-function-for-deep-learning-neural-networks/
     model.add(Dense(20, activation='relu'))
     model.add(Dense(5, activation='softmax'))
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])  # https://www.tutorialspoint.com/keras/keras_model_compilation.htm
     print(model.summary())
-    history = model.fit(trainX, trainy, validation_data=(testX, testy),batch_size=1, epochs=100, verbose=1)
+    history = model.fit(trainX, trainy, validation_data=(testX, testy), batch_size=1, epochs=100, verbose=1)
 
-    pyplot.plot(history.history['accuracy'], label='train')
-    pyplot.plot(history.history['val_accuracy'], label='test')
+    pyplot.plot(history.history['accuracy'], label='train')  # https://matplotlib.org/3.1.1/api/_as_gen/matplotlib.pyplot.html#:~:text=pyplot%20is%20a%20state%2Dbased,%2C%200.1)%20y%20%3D%20np.
+    pyplot.plot(history.history['val_accuracy'], label='test')  # https://www.geeksforgeeks.org/pyplot-in-matplotlib/
     pyplot.show()
     scores = model.evaluate(trainX, trainy, verbose=1)
     print("%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
     model.save("personality_model.h5")
     print("Model saved to disk")
 
-def predict_model(model_type,input_data,le):
-    model = load_model(model_type+'.h5')
+
+def predict_model(model_type, input_data, le):
+    model = load_model(model_type + '.h5')
     print(model.summary())
-    test_input = np.reshape(input_data, (1,10))
+    test_input = np.reshape(input_data, (1, 10))
     test_output = model.predict(test_input, verbose=1)
     output_data = np.argmax(test_output, 1)
     output_class = le.inverse_transform(output_data)
     return output_class[0]
 
+
 if __name__ == "__main__":
-    app = Flask(__name__)
+    app = Flask(__name__)  # https://www.fullstackpython.com/flask.html
+
+
     # for model training comment new_input_array and predict_model fields
     # for model prediction comment each model
     @app.route('/learn')
@@ -127,10 +136,9 @@ if __name__ == "__main__":
         for i in range(10):
             input_array.append(int(my_dict[str(i)]))
         trainX, trainy, testX, testy, le = prepare_data("Personality-Trait")
-        #personality_model(trainX, trainy, testX, testy)
+        # personality_model(trainX, trainy, testX, testy)
         predicted_output = predict_model("personality_model", input_array, le)
         return predicted_output
 
+
     app.run(host='0.0.0.0', port=5000, debug=True)
-
-
