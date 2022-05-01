c6ede324069fb96655a1eb6f54af32456044b00a
Mathushaan Vigneswararajah <mathushaan.20200207@iit.ac.lk>
2022-05-01 22:10:42 +0530

    Chatbot added to Teacher Page

--- front-end/Learnality/my-app/src/components/TeacherHome.jsx
+++ front-end/Learnality/my-app/src/components/TeacherHome.jsx
@@ -9,6 +9,19 @@ import { TeacherSideBar } from "../components/TeacherSideBar";
 
 
 export class TeacherHome extends React.Component{
+    componentDidMount() {
+        
+        (function(d, m){
+            var kommunicateSettings = 
+                {"appId":"1903feb6bdd7ce29e3a1447976193ff7","popupWidget":true,"automaticChatOpenOnNavigation":true};
+            var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+            s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+            var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+            window.kommunicate = m; m._globals = kommunicateSettings;
+        })(document, window.kommunicate || {});
+    /* NOTE : Use web server to view HTML files as real-time update will not work if you directly open the HTML file in the browser. */
+          
+}
     render(){
         return(
             <div className="home">
